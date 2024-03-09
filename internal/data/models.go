48565800e4baa4a43ecccfdb419b30a34af254db
noah-d-whitney <me@noahwhitney.com>
2024-03-09 04:01:43 -0500

    permissions model

--- internal/data/models.go
+++ internal/data/models.go
@@ -9,15 +9,17 @@ var ErrRecordNotFound = errors.New("record not found")
 var ErrEditConflict = errors.New("edit conflict")
 
 type Models struct {
-	Users   UserModel
-	Players PlayerModel
-	Tokens  TokenModel
+	Users       UserModel
+	Players     PlayerModel
+	Tokens      TokenModel
+	Permissions PermissionModel
 }
 
 func NewModels(initDb *sql.DB) Models {
 	return Models{
-		Users:   UserModel{db: initDb},
-		Players: PlayerModel{db: initDb},
-		Tokens:  TokenModel{db: initDb},
+		Users:       UserModel{db: initDb},
+		Players:     PlayerModel{db: initDb},
+		Tokens:      TokenModel{db: initDb},
+		Permissions: PermissionModel{db: initDb},
 	}
 }
