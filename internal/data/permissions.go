8e9825cb05c55f7d64f170e61adf982b9f368ef0
noah-d-whitney <me@noahwhitney.com>
2024-03-10 16:14:19 -0400

    assign user permissions

--- internal/data/permissions.go
+++ internal/data/permissions.go
@@ -3,6 +3,7 @@ package data
 import (
 	"context"
 	"database/sql"
+	"github.com/lib/pq"
 	"time"
 )
 
@@ -54,4 +55,16 @@ func (m *PermissionModel) GetAllForUser(userID int64) (Permissions, error) {
 	}
 
 	return permissions, nil
+}
+
+func (m *PermissionModel) AddForUser(userID int64, codes ...string) error {
+	stmt := `
+		INSERT INTO users_permissions
+		SELECT $1, permissions.id FROM permissions WHERE permissions.code = ANY($2)`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := m.db.ExecContext(ctx, stmt, userID, pq.Array(codes))
+	return err
 }
